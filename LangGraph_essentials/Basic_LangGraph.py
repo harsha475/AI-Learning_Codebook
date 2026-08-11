ab8cba470e048ca736087368c35b936e7412be26
harsha475 <175298473+harsha475@users.noreply.github.com>
2026-08-11 23:20:40 +0530

    Update Basic_LangGraph.py

--- LangGraph_essentials/Basic_LangGraph.py
+++ LangGraph_essentials/Basic_LangGraph.py
@@ -1,1 +1,27 @@
-m
+from langgraph.graph import StateGraph, START, END
+from langchain_openai import ChatOpenAI
+from typing import TypedDict
+
+model = ChatOpenAI()
+# create a state class
+class State(TypedDict):
+    user: str
+    answer: str
+
+def single_response(state: State):
+    response = model.invoke(state["user"])
+    return {"answer": response.content}
+    
+#build a state graph
+builder = StateGraph(State)
+#adding nodes
+
+builder.add_node("single_response", single_response)
+builder.add_edge(START, "single_response")
+builder.add_edge("single_response", END)
+
+result = builder.compile()
+data = input("ask a question:")
+final_answer = result.invoke(data)
+print(final_answer)
+
